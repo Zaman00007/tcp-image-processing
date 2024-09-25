e65dadd2eae8ec483c5cf695e7ec6f383a1b642b
Zaman00007 <shahidzaman75724@gmail.com>
2024-09-25 20:05:57 +0530

    Implement TCP server based image processor

---
+++ TcpImageClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+class TcpImageClient
+{
+    static void Main(string[] args)
+    {
+        string imagePath = "input_image.jpg"; 
+
+        if (!File.Exists(imagePath))
+        {
+            Console.WriteLine("Image file not found!");
+            return;
+        }
+
+        byte[] imageBytes = File.ReadAllBytes(imagePath); 
+
+        using (TcpClient client = new TcpClient("127.0.0.1", 5000)) 
+        using (NetworkStream stream = client.GetStream())
+        {
+            Console.WriteLine("Connected to the server. Sending image...");
+
+            
+            byte[] lengthBuffer = BitConverter.GetBytes(imageBytes.Length);
+            stream.Write(lengthBuffer, 0, lengthBuffer.Length);
+
+            
+            stream.Write(imageBytes, 0, imageBytes.Length);
+            Console.WriteLine("Image sent successfully.");
+        }
+    }
+}
---
+++ TcpImageProcessing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing; 
+
+class TcpImageServer
+{
+    static void Main(string[] args)
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Server is running...");
+
+        while (true)
+        {
+            using (TcpClient client = listener.AcceptTcpClient())
+            using (NetworkStream stream = client.GetStream())
+            {
+                Console.WriteLine("Client connected.");
+
+                
+                byte[] lengthBuffer = new byte[4];
+                stream.Read(lengthBuffer, 0, lengthBuffer.Length);
+                int imageLength = BitConverter.ToInt32(lengthBuffer, 0);
+
+                
+                byte[] imageBytes = new byte[imageLength];
+                stream.Read(imageBytes, 0, imageLength);
+                Console.WriteLine("Image received. Processing...");
+
+                
+                using (Image<Rgba32> image = Image.Load<Rgba32>(new MemoryStream(imageBytes)))
+                {
+                    
+                    image.Mutate(x => x.Invert());
+
+                    
+                    image.Save("output_image.jpg");
+                    Console.WriteLine("Image processed and saved as output_image.jpg.");
+                }
+            }
+        }
+    }
+}
